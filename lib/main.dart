5c91645eaf5f227b2cbd5da2014a715b46665b55
jubayed004 <mohammadjubayed.islam97@gmail.com>
2025-01-21 01:31:23 +0600

    spme change

--- lib/main.dart
+++ lib/main.dart
@@ -37,14 +37,33 @@ List<todo> todos =[];
       appBar: AppBar(
         title: Text("ToDo List App"),
         centerTitle: true,
+        actions: [
+          IconButton(onPressed: (){
+            todos.clear();
+            if(mounted){
+              setState(() {
+
+              });
+            }
+          }, icon: Icon(Icons.delete_sweep))
+        ],
       ),
       body: ListView.builder(
         itemCount: todos.length,
           itemBuilder: (context, index)=>ListTile(
+            onLongPress: (){
+              todos[index].isDone=!todos[index].isDone;
+              setState(() {
+              });
+            },
+            leading: todos[index].isDone?Icon(Icons.check_box_outline_blank):Icon(Icons.check_box),
             title: Text(todos[index].title),
 
             trailing: IconButton(onPressed: (){
+                todos.removeAt(index);
+                setState(() {
 
+                });
             }, icon: Icon(Icons.delete)),
           )
 
@@ -62,8 +81,17 @@ List<todo> todos =[];
                   child: Column(
                     children: [
                       TextFormField(
+                        validator: (value){
+                          if(value!.isEmpty){
+                          return 'Please enter some text';
+                          }
+                          return null;
+                        },
                         controller: todoTextEditingController,
-                        decoration: const InputDecoration(
+                        decoration:  InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20)
+                          ),
                           labelText: "Enter number",
                           isDense: true,
                           focusedBorder: OutlineInputBorder(
@@ -75,7 +103,6 @@ List<todo> todos =[];
                             borderSide: BorderSide(color: Colors.black,width: 2)
                           ),
 
-
                         ),
 
                       ),
@@ -83,7 +110,9 @@ List<todo> todos =[];
                       ElevatedButton(
                         onPressed: () {
                           todos.add(todo(todoTextEditingController.text.trim(), false));
-                          todoTextEditingController.clear();
+                          if(_formKey.currentState!.validate()){
+                            todoTextEditingController.clear();
+                          }
                           setState(() {
 
                           });
@@ -107,5 +136,6 @@ List<todo> todos =[];
 class todo{
   String title;
   bool isDone;
+  
   todo(this.title,this.isDone);
 }
